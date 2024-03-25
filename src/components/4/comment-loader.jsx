00b7a059527f7efdcd707e2ae19049872edf2a96
Dasad-8 <144725256+Dasad-8@users.noreply.github.com>
2024-03-25 19:51:26 +0100

    Create comment-loader.jsx
    Create comment-loader.jsx

---
+++ src/components/4/comment-loader.jsx
@@ -0,0 +1,21 @@
+import React from "react;
+import ContentLoader from "react-content-loader";
+
+const CommentLoader = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={396}
+    height={332}
+    viewBox="0 0 396 332"
+    backgroundColor="#dbdbdb"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="198" cy="16" r="41" /> 
+    <rect x="50" y="91" rx="0" ry="0" width="135" height="22" /> 
+    <rect x="50" y="128" rx="0" ry="0" width="295" height="22" /> 
+    <rect x="50" y="160" rx="0" ry="0" width="295" height="110" />
+  </ContentLoader>
+)
+
+export default CommentLoader;
