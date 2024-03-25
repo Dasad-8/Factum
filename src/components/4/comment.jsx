463d0d1d95e8d56f6469d5ef4757d382f9c99081
Dasad-8 <144725256+Dasad-8@users.noreply.github.com>
2024-03-25 21:31:17 +0100

    Update comment.jsx
    Update

--- src/components/4/comment.jsx
+++ src/components/4/comment.jsx
@@ -6,9 +6,9 @@ function Comment (props) {
 
     return <>
     
-            <img className='comment-img' src={comments.user_img} alt="User profile" /> 
+            <img className='comment-img' src={props.userImg} alt="User profile" /> 
             <div className='rating-comment'>
-                <p>5 з 5</p>
+                <p>{props.userRating} з 5</p>
                 <div className='stars-comment'>
                     <img src="./img/star-blue2.png" alt="error" />
                     <img src="./img/star-blue2.png" alt="error" />
@@ -17,8 +17,8 @@ function Comment (props) {
                     <img src="./img/star-blue2.png" alt="error" />
                 </div>
             </div>
-            <h6>Костянтин Яневський</h6>
-            <p>Сервіс дуже непоганий, старанність та відповідальність присутні , прозорий формат роботи, можно спостерігати за всіма єтапами праці .</p>
+            <h6>{props.userName}</h6>
+            <p>{props.textComment}</p>
         
     </>
 }
