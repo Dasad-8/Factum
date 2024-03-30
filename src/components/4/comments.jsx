2879a6dc1244d8a84ff1f766ef52217f946f4718
Dasad-8 <dasad6970@gmail.com>
2024-03-30 14:50:11 +0200

    update

--- src/components/4/comments.jsx
+++ src/components/4/comments.jsx
@@ -5,32 +5,6 @@ import React, {useState, useEffect} from 'react';
 
 function Comments (props) {
 
-    const [comments, setComments] = useState([]);
-
-    useEffect(() =>{
-        fetchComments();
-    }, []);
-
-    const fetchComments = async() =>{
-        fetch ("./comments-user.json")
-        .then(response => response.json())
-        .then(comments =>{console.log('Received data:', comments);
-        setComments(comments)});
-    }
-
-    return <>
-        <section className='wr'>
-            <h2>Відгуки в Google</h2>
-            <p className='rating'>4.92</p>
-            <div className='stars'>
-                <img src="./img/star-blue.png" alt="error" />
-                <img src="./img/star-blue.png" alt="error" />
-                <img src="./img/star-blue.png" alt="error" />
-                <img src="./img/star-blue.png" alt="error" />
-                <img src="./img/star-blue.png" alt="error" />
-            </div>
-            <p className='answer-counter'>на основі 244 відповідей</p>
-{
 /*             <ol className='comment-list'>{ (comments.length === 0) ? <> 
                 <CommentLoader /> 
                 <CommentLoader /> 
@@ -46,6 +20,33 @@ function Comments (props) {
                
                 <button className='comments-right-arrow'><img src="./img/right-arrow.png" alt="error" /></button>}
             </ol> */
+
+    const [comments, setComments] = useState([]);
+
+    useEffect(() =>{
+        fetchComments();
+    }, []);
+
+    const fetchComments = async() =>{
+        fetch ("./comments-user.json")
+        .then(response => response.json())
+        .then(comments =>{console.log('Received data:', comments);
+        setComments(comments)});
+    }
+
+    return <>
+        <section className='wr'>
+        <h2>Відгуки в Google</h2>
+            <p className='rating'>4.92</p>
+            <div className='stars'>
+                <img src="./img/star-blue.png" alt="error" />
+                <img src="./img/star-blue.png" alt="error" />
+                <img src="./img/star-blue.png" alt="error" />
+                <img src="./img/star-blue.png" alt="error" />
+                <img src="./img/star-blue.png" alt="error" />
+            </div>
+            <p className='answer-counter'>на основі 244 відповідей</p>
+
         </section>
         
     </>
