0681d7baa20118ef180c805baad8e19000af530b
Dasad-8 <dasad6970@gmail.com>
2024-04-13 01:07:58 +0200

    update

--- src/components/4/comment.jsx
+++ src/components/4/comment.jsx
@@ -3,22 +3,32 @@ import './comment.css';
 function Comment (props) {
 
 
-
     return <>
-    
-            <img className='comment-img' src={props.userImg} alt="User profile" /> 
+        <section>
+        <img className='comment-img' src={props.userImg} alt="User profile" /> 
             <div className='rating-comment'>
                 <p>{props.userRating} з 5</p>
-                <div className='stars-comment'>
+                <div className='stars-comment'>{(props.userRating === '1') ?
+                    <img src="./img/star-blue2.png" alt="error" /> : (props.userRating === '2') ?<>
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" /></> : (props.userRating === '3') ? <>
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" /></> : (props.userRating === '4') ? <>
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" /></> : (props.userRating === '5') ? <>
                     <img src="./img/star-blue2.png" alt="error" />
                     <img src="./img/star-blue2.png" alt="error" />
                     <img src="./img/star-blue2.png" alt="error" />
                     <img src="./img/star-blue2.png" alt="error" />
-                    <img src="./img/star-blue2.png" alt="error" />
+                    <img src="./img/star-blue2.png" alt="error" /></> : null}
                 </div>
             </div>
             <h6>{props.userName}</h6>
             <p>{props.textComment}</p>
+        </section>
         
     </>
 }
